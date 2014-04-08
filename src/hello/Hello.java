574c99a009964f7d34d664680538a7495adb220a
aoyagi <aoyagi@m20601.tsuda.ac.jp>
2014-04-08 15:47:34 +0900

    最初

---
+++ src/hello/Hello.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+/**
+ *
+ * @author aoyagi
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}
